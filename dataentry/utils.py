cedda8be11743678c0ac15b4f5b88fd108341861
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-11-02 10:22:15 +0530

    Fetch custom Django models dynamically for import feature

---
+++ dataentry/utils.py
@@ -0,0 +1,13 @@
+from django.apps import apps
+
+def get_all_custom_models():
+    default_models = ['ContentType', 'Session', 'Permission', 'Group','LogEntry','User']
+    #try to get all the apps
+    custom_models = []
+    for model in apps.get_models():
+        if model.__name__ not in default_models:
+            custom_models.append(model.__name__)
+        
+        
+    return custom_models
+ 

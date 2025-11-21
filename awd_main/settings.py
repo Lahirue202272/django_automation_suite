e9c30f181e8bab9a8bf964cc98175d706d46a198
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-11-21 17:48:03 +0530

    Login and registartion features

--- awd_main/settings.py
+++ awd_main/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'dataentry',
     'uploads',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -150,3 +152,6 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Automate with Django <balagamageniroshan@gmail.com>'
 DEFAULT_TO_EMAIL = 'bg.lahiruniroshan@gmail.com'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- awd_main/urls.py
+++ awd_main/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('dataentry/', include('dataentry.urls')),
     path('',views.home, name='home'),
     path('celery-test/', views.celery_test),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
